ff278de7fb8adf1b3c57e1e55af94bbd771faf82
Pascual <pasperrui@alu.edu.gva.es>
2025-02-13 16:36:47 +0100

    Rutas definidas para CRUD y controlador

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use Illumninate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AeronaveController;
 
+Route::apiResource('aeronaves', AeronaveController::class);
 Route::get('/user', function (Request $request) {
 	return $request->user();
 });
